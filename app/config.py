281fda03bc99ba0456b7711214d54392bde18d45
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-02-16 12:57:14 +0000

    adding telegram config

---
+++ app/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    telegram_bot_token: str
+    telegram_chat_id: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
